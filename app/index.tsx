8b3254973ce8e655666eafffb642fe0e4077f9b5
PROGRAMADOR EXTERNO <programador_externo@zermat.com.mx>
2025-08-25 23:24:09 -0600

    feat: update color

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { ScrollView, Text, View } from "react-native";
 
 export default function HomeScreen() {
    return (
-      <ScrollView className="flex-1 bg-red-400">
+      <ScrollView className="flex-1 bg-cyan-400">
          {/* Hero Section */}
          <View className="bg-gradient-to-r from-blue-600 to-purple-600 p-6 m-4 rounded-xl">
             <Text className="text-white text-2xl font-bold text-center mb-2">¡Bienvenido! 🚀</Text>
